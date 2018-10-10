d8031dfab5cdcb1a91cc0f91a140121844e87f6c
Yehonatan-Bar <43821476+Yehonatan-Bar@users.noreply.github.com>
2018-10-10 20:11:23 +0300

    java

---
+++ Cinema/src/SeatBuyingProgram.java
@@ -0,0 +1,34 @@
+//import java.util.Scanner;
+//
+//public class SeatBuyingProgram {
+//
+//	public static void main(String[] args) {
+//		// TODO Auto-generated method stub
+//		int min = 70;
+//		int max = 90;
+//		// int x;
+//
+//		Scanner chairs = new Scanner(System.in);
+//		Scanner line = new Scanner(System.in);
+//		Scanner column = new Scanner(System.in);
+//		SeatBuying.setPrice((Math.random() * (max - min)) + min);
+//
+//		System.out.print("Please insert the  number of chairs you want to buy: ");
+//		int x = chairs.nextInt();
+//		
+//		for (int i = 0; i < x; i++) {
+//			System.out.print("Please insert the chair line you want: ");
+//			int chairline = line.nextInt();
+//			System.out.print("Please insert the chair column you want: ");
+//			int chaircolumn = line.nextInt();
+//			chairs[chairline][chaircolumn].isToken(true);
+////isToken
+//
+//			
+//			
+//		} 
+//
+//		
+//
+//	}
+//}
