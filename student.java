66c21774c7c3edceb5f5aaea4dcdcb526fcc4436
Yehonatan-Bar <43821476+Yehonatan-Bar@users.noreply.github.com>
2018-10-14 21:24:21 +0300

    Add files via upload

---
+++ student.java
@@ -0,0 +1,14 @@
+package student;
+
+public class student {
+	private int score;
+	private int age;
+	static private int sumScore = 0;
+	static private int sumAge = 0;
+	static private int sumStudent = 0;
+
+
+	
+	
+
+}
