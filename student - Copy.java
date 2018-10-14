f5f0c8d9b2d62f7ee7c18b00403e248fc68fc13b
Yehonatan-Bar <43821476+Yehonatan-Bar@users.noreply.github.com>
2018-10-14 21:35:26 +0300

    Add files via upload

---
+++ student - Copy.java
@@ -0,0 +1,69 @@
+package student;
+
+public class student {
+	private int score;
+	private int age;
+	static private int sumScore = 0;
+	static private int sumAge = 0;
+	static private int sumStudent = 0;
+
+	public student(int score, int age) {
+		super();
+		this.score = score;
+		this.age = age;
+		student.sumScore += score;
+		student.sumAge += age;
+
+	}
+
+	public int getScore() {
+		return score;
+	}
+
+	public static int avgScore() {
+		return sumScore / sumStudent;
+	}
+
+	public static int avgAge() {
+		return sumAge / sumStudent;
+	}
+
+	// ------------------------------------------------------------//
+	public void setScore(int score) {
+		this.score = score;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public static int getSumScore() {
+		return sumScore;
+	}
+
+	public static void setSumScore(int sumScore) {
+		student.sumScore = sumScore;
+	}
+
+	public static int getSumAge() {
+		return sumAge;
+	}
+
+	public static void setSumAge(int sumAge) {
+		student.sumAge = sumAge;
+	}
+
+	public static int getSumStudent() {
+		return sumStudent;
+	}
+
+	public static void setSumStudent(int sumStudent) {
+		student.sumStudent = sumStudent;
+	}
+	// ------------------------------------------------------------//
+
+}
