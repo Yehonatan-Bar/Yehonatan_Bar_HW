89d0ace8b1f72ea83071250299cef8f194a8f363
Yehonatan Bar <43821476+Yehonatan-Bar@users.noreply.github.com>
2018-11-04 21:53:44 +0200

    Add files via upload

---
+++ Passenger.java
@@ -0,0 +1,23 @@
+
+public class Passenger extends Product {
+	
+private double flightPrice;
+private int maxCaseWeight;
+P
+
+	
+
+}
+
+
+
+ /*
+
+מחיר טיסה
+מספר קילו מורשה עבור מזוודה
+מזוודה
+מחיר עבור כל קילו נוסף
+
+*/
+
+}
